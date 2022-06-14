b878d0006877f7cfb829f54f2001e895b804a88e
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-06-14 20:57:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,24 @@ const prompt = 'My name is Albert Einstein. I am a theoretical physicist who dev
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const cohere = require('cohere-ai');
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.append('Access-Control-Allow-Origin','*')
 let data = {
-    context: req.query.context || prompt,
+    prompt: req.query.context || prompt,
                 temperature: req.query.temperature || 0.8,
-                token_max_length: req.query.token_max_length || 40,
-                top_p: req.query.top_p || 1,
-                stop_sequence: req.query.stop_sequence || '\n',
+                max_tokens: req.query.token_max_length || 40,
+                p: req.query.top_p || 1,
+                stop_sequences: req.query.stop_sequence || '\n',
 }
-let params = Object.entries(data).map(([key, val]) => `${key}=${encodeURIComponent(val)}`).join('&')
+cohere.init(process.env.COHERE_AI_KEY);
 
-  axios.post(`http://api.vicgalle.net:5000/generate?${params}`)
+  // Hit the `generate` endpoint on the `large` model
+  const generateResponse = await cohere.generate("medium", data);
+let params = Object.entries(data).map(([key, val]) => `${key}=${encodeURIComponent(val)}`).join('&')
+ await res.send(generateResponse.text)
+/*  axios.post(`http://api.vicgalle.net:5000/generate?${params}`)
   .then(function (response) {
     console.log(response.data.text)
 res.send(response.data.text)
@@ -24,7 +29,7 @@ res.send(response.data.text)
   .catch(function (error) {
     console.log(error);
   })
-  
+ */ 
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
